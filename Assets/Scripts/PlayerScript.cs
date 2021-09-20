824f2ac88a741031079b62bf7aed379dcb06bb40
DannyAdamFrancis <90899391+DannyAdamFrancis@users.noreply.github.com>
2021-09-20 16:25:45 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoJump();
+        DoMove();
+        print(isGrounded);
+    }
+
+    void DoJump()
+    {
+        Vector2 velocity = rb.velocity;
+
+        // check for jump
+        if (Input.GetKey("space") && (isGrounded == true))
+
+        {
+            if (velocity.y < 0.01f)
+            {
+                velocity.y = 5f;    // give the player a velocity of 5 in the y axis
+
+            }
+        }
+
+        rb.velocity = velocity;
+
+    }
+
+    void DoMove()
+    {
+        Vector2 velocity = rb.velocity;
+
+        // stop player sliding when not pressing left or right
+        velocity.x = 0;
+
+        // check for moving left
+        if (Input.GetKey("a"))
+        {
+            velocity.x = -7;
+        }
+
+        // check for moving right
+        if (Input.GetKey("d"))
+        {
+            velocity.x = 7;
+        }
+        rb.velocity = velocity;
+
+
+    }
+
+
+    
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+}
+
