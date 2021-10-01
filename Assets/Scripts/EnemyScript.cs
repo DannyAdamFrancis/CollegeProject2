2595624da26a3c486bc7aa8ca8d869ae07df76af
DannyAdamFrancis <90899391+DannyAdamFrancis@users.noreply.github.com>
2021-10-01 14:17:43 +0100

    the enemy has animation now
    GREAT

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+        
 public class EnemyScript : MonoBehaviour
+      
 {
-    // Start is called before the first frame update
-    void Start()
+       public GameObject Player;
+
+// Start is called before the first frame update
+void Start()
     {
         
     }
