dfb7dd8ee935ae017a6ddae386259fe10ad11807
DannyAdamFrancis <90899391+DannyAdamFrancis@users.noreply.github.com>
2021-10-01 14:36:12 +0100

    idk
    new

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -2,15 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-        
 public class EnemyScript : MonoBehaviour
-      
-{
-       public GameObject Player;
 
-// Start is called before the first frame update
-void Start()
+    
+{
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
     {
         
     }
